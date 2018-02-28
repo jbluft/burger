56c0e93e18af0cd2781499f2070a4288092d8bde
jbluft <jbluft@gmail.com>
2018-02-28 16:23:17 -0500

    connectionrevert

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ var routes = require("./controllers/burgers_Controller.js");
 
 app.use("/", routes);
 
-app.listen(process.env.PORT, function() {
+app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
